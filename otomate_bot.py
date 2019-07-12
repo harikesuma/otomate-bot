2c9a3e137bacafa5dc45d0608c309447f992109a
harikesuma <harikesuma98@gmail.com>
2019-07-12 13:42:33 +0800

    fixing /read command + adding echo command

--- otomate_bot.py
+++ otomate_bot.py
@@ -8,7 +8,8 @@ from selenium.webdriver.common.keys import Keys
 
 # informasi logging dengan fromat waktu,nama,critical,pesan
 logging.basicConfig(filename='otomate_bot.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+                    level=logging.INFO
+                    )
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
@@ -16,34 +17,39 @@ def start(bot, update):
 
 def read(bot, update):
     browser = webdriver.Chrome('C:\Program Files (x86)\Chrome Driver\chromedriver.exe')
-    browser.get('https://www.outline.com')
+    browser.get('https://outline.com/')
     linkbar = browser.find_element_by_id('source')
-    linkbar.send_keys()
+    link = update.message.text
+    linkbar.send_keys(link.replace("/read ",''))
     linkbar.send_keys(Keys.ENTER)
     time.sleep(20)
-    bot.send_message(chat_id=update.message.chat_id, text=browser.current_url)
+    bot.send_message(chat_id=update.message.chat_id, text= "Here your link " + browser.current_url)
 
 
 def about(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text="Edited by Hari Kesuma")
 
-def img(bot, update):
-    directory = "C:Users/Hari Kesuma/Pictures/MyerSplash"
-    random_image = random.choice(os.listdir(directory))
-    random_image =  directory + random_image
-    bot.send_photo(chat_id=update.message.chat_id, photo=random_image)
+# def img(bot, update):
+#     directory = "C://Users/Hari Kesuma/Pictures/MyerSplash"
+#     random_image = random.choice(os.listdir(directory))
+#     random_image =  directory + random_image
+#     bot.send_photo(chat_id=update.message.chat_id, photo=random_image)
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
 def main():
     # Create updater and pass in Bot's auth key.
-    updater = Updater("TOKEN")
+    updater = Updater(token='856062988:AAElVBonRqiXe95Yh9WZdazhMv5l2_xzmTo')
     # Get dispatcher to register handlers
     dispatcher = updater.dispatcher
     # answer commands
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('read', read))
     dispatcher.add_handler(CommandHandler('about', about))
-    dispatcher.add_handler(CommandHandler('img', img))
+    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    # dispatcher.add_handler(CommandHandler('img', img))
     # start the bot
     updater.start_polling()
     # Stop
